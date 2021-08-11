5b415bafa7b74a0ef81ed7c3011b21c963564372
FerroDude <pedroferrodude@hotmail.com>
2021-08-11 10:49:49 +0100

    Finished iteration 6

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length || pos < 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    const lastIndex = this.items.length - 1;
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[lastIndex];
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (!this.items.length) {
+      return 0;
+    } else {
+      return this.items.reduce(reducer);
+    }
+  }
 
   avg() {}
 }
